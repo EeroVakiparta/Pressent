def5517b76680534fa73bac63336ba048e20bb58
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-11-28 16:01:44 +0200

    Slowly forming the "button idea"
    - decided to remove 6th button to make room for servo

--- pressent/pressent.ino
+++ pressent/pressent.ino
@@ -16,6 +16,7 @@
 #include <Fonts/FreeSans9pt7b.h>
 #include <Fonts/FreeSans18pt7b.h>
 #include <Fonts/FreeSans12pt7b.h>
+#include <Bounce2.h>
 
 #define OLED_RESET 0  // GPIO0
 #define SCREEN_WIDTH 128
@@ -28,10 +29,24 @@
 // Using old project as reference (feed-o-pony) let the copy/paste commence!!
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Pro micro has funky numbers, no not mind. also look closely needed pin sequence:
+// Pro micro has funky numbers, no not mind. Also look closely needed pin sequence:
 // :D IN1-IN3-IN2-IN4
 AccelStepper stepper1(FULLSTEP, 15, 16, 14, 10);
 AccelStepper stepper2(FULLSTEP, 21, 19, 20, 18);
+
+enum menu {
+  start,
+  program
+};
+
+enum startMenuSelection {
+  startProgram,
+  editProgram,
+  deleteProgram
+};
+
+menu currentMenu = start;
+startMenuSelection currentStartMenuSelection = startProgram;
 
 // Setup
 const int stepsNeededForRevolution = 4096;  // 28BYJ-48 used, for other change the value
@@ -39,12 +54,44 @@ int defaultMaxSpeed = 1000; // read the values over 1000 would be unreliable?
 int defautlAcceleration = 500.0; // max acceleration (steps per second^2)
 
 // fags
-int running = 0;
 int currenctCommand = 0;
 int nextCommandNeeded = 1;
 
+//
+byte commands[100];
+
+// Pin definitnos
+const int upButtonPin = 4;
+const int downButtonPin = 5;
+const int selectButtonPin = 6;
+const int cancelButtonPin = 7;
+const int backButtonPin = 8;
+/*
+    Pinmap:
+    2-3 screen
+    4-8 buttons
+    9 free, possibly for servo
+    10-21 steppers ( note: pins 11,12,13 and 17 do not excist)
+*/
+
+int upButton;
+int downButton;
+int selectButton;
+int cancelButton;
+int backButton;
+
+
+
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(upButtonPin, INPUT);
+  pinMode(downButtonPin, INPUT);
+  pinMode(selectButtonPin, INPUT);
+  pinMode(cancelButtonPin, INPUT);
+  pinMode(backButtonPin, INPUT);
+
 
   //Stepper setups
   stepper1.setMaxSpeed(defaultMaxSpeed);
@@ -67,39 +114,70 @@ void setup() {
 
   stepper1.setCurrentPosition(0);
   stepper2.setCurrentPosition(0);
+  delay(10);
 }
 
 
+
+
 void loop() {
-  if (nextCommandNeeded && !running) {
-    executeCommand(currenctCommand);
-    newCommandPrint(currenctCommand);
-    delay(1000);
-    showSteps(stepsNeededForRevolution);
-    delay(20);
+
+  /*
+    First of all try to check if program excecution is going on and try not to disturb it as
+    doing anything else while operating stepper motors will distract or delay their operation.
+
+  */
+
+  // 1. Make sure program is not running
+  if (!stepper1.isRunning() && !stepper2.isRunning()) {
+    
+    // 2. Check if we are in main menu or programming 
+    if (menu == start) {
+      upButton = digitalRead(upButtonPin);
+      downButton = digitalRead(downButton);
+      selectButton = digitalRead(selectButton);
+      cancelButton = digitalRead(cancelButton);
+      backButton = digitalRead(backButton);
+
+
+    }else if (menu == program){
+
+      
+    }
+
+    
+
+    if (nextCommandNeeded) {
+      executeCommand(currenctCommand);
+      newCommandPrint(currenctCommand);
+      delay(1000);
+      showSteps(stepsNeededForRevolution);
+      delay(20);
+    }
   }
 
 
+
+ // run the steppers untill they reach their destination
   if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0) {
-    //stepper1.moveTo(-stepper1.currentPosition());
-    //stepper2.moveTo(-stepper1.currentPosition());
     currenctCommand++;
-    running = 0;
   } else {
     stepper1.run();
     stepper2.run();
-    running = 1;
   }
 
+
+  // temporary for debugging:
   if (currenctCommand > 5) {
     currenctCommand = 0;
   }
+
 }
 
 // TODO: make better structure
 void executeCommand(int commandIndex) {
-Serial.print("executeCommand: ");
-Serial.println(commandIndex);
+  Serial.print("executeCommand: ");
+  Serial.println(commandIndex);
 
   switch (commandIndex) {
     case 0:
