07cee05d7b0e81d9bfd115f3f6e17503875d9abd
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-11-28 12:33:20 +0200

    Introduced the screen + setup
    - note to self: terrible to troubleshoot the screen

--- pressent/pressent.ino
+++ pressent/pressent.ino
@@ -1,36 +1,84 @@
 // go for pro micro, nano doesnt have enough I/Os for expansion
 
 //Notes: - maybe should do the truns on half speed just in case.
-//       - do I need to introduce "slow acceleration" ; maybe not cause of high torgue
 
 //TODO:  - check out https://github.com/mateusjunges/accel-stepper-with-distances
+//       - check out https://forum.arduino.cc/index.php?topic=515370.0
 
 const int stepsNeededForRevolution = 4096;  // 28BYJ-48 used, for other change the value
 
 #include <AccelStepper.h>
-// #include <Stepper.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
+
+#define OLED_RESET 0  // GPIO0
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
 
 // Define step constant
 #define FULLSTEP 8
 
+// See your proper pins for the screen, D2&3 for por micro in this case
+// Using old project as reference (feed-o-pony) let the copy/paste commence!!
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Pro micro has funky numbers, no not mind. also look closely needed pin sequence:
 // :D IN1-IN3-IN2-IN4
 AccelStepper stepper1(FULLSTEP, 15, 16, 14, 10);
 
 void setup() {
+  Serial.begin(9600);
+  
+  //Stepper setup
   stepper1.setMaxSpeed(1000);
   stepper1.setAcceleration(500.0); // max acceleration (steps per second^2)
  // stepper1.setSpeed(50); // steps per second
-  
+
+ //Screen setup
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+  display.clearDisplay();
+  display.setFont(&FreeSans9pt7b);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  delay(1000);
+  Serial.println("setup complete");
 }
 
 void loop() {
+  Serial.println("loop started");
   // warmup, drive revolution forward and chill
   stepper1.setCurrentPosition(0);
+  driving();
   stepper1.moveTo(stepsNeededForRevolution);
   stepper1.runToPosition();
+  showSteps(stepsNeededForRevolution);
   delay(2000);
-
   
+Serial.println("loop ended");
+}
+
+void driving(void) {
+  Serial.println("driving");
+  display.clearDisplay();
+  display.setCursor(16, 22);     // x,y
+  display.print("~DRIVIG~");
+  display.display();
+  delay(10);
+}
+
+void showSteps(int stepCount) {
+  Serial.println("showSte");
+  display.clearDisplay();
+  display.setCursor(16, 22);     // x,y
+  display.print(stepCount);
+  display.print(" steps");
+  display.display();
+  delay(10);
 }
