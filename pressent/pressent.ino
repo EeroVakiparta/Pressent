10edce22904aa7565fe913ee51cd87cac36095bb
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-11-28 14:07:28 +0200

    Forward, back, left & right drive -commands

--- pressent/pressent.ino
+++ pressent/pressent.ino
@@ -6,7 +6,7 @@
 //       - check out https://forum.arduino.cc/index.php?topic=515370.0
 //       - check out https://github.com/thomasfredericks/Bounce2
 
-const int stepsNeededForRevolution = 4096;  // 28BYJ-48 used, for other change the value
+
 
 #include <AccelStepper.h>
 #include <SPI.h>
@@ -33,18 +33,26 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 AccelStepper stepper1(FULLSTEP, 15, 16, 14, 10);
 AccelStepper stepper2(FULLSTEP, 21, 19, 20, 18);
 
+// Setup
+const int stepsNeededForRevolution = 4096;  // 28BYJ-48 used, for other change the value
+int defaultMaxSpeed = 1000; // read the values over 1000 would be unreliable?
+int defautlAcceleration = 500.0; // max acceleration (steps per second^2)
+
+// fags
 int running = 0;
+int currenctCommand = 0;
+int nextCommandNeeded = 1;
 
 void setup() {
   Serial.begin(9600);
 
   //Stepper setups
-  stepper1.setMaxSpeed(1000);
-  stepper1.setAcceleration(500.0); // max acceleration (steps per second^2)
-  //stepper1.setSpeed(300); // steps per second
-  stepper2.setMaxSpeed(1000);
-  stepper2.setAcceleration(500.0); // max acceleration (steps per second^2)
-  //stepper2.setSpeed(600); // steps per second
+  stepper1.setMaxSpeed(defaultMaxSpeed);
+  stepper1.setAcceleration(defautlAcceleration);
+  //stepper1.setSpeed(500); // steps per second
+  stepper2.setMaxSpeed(defaultMaxSpeed);
+  stepper2.setAcceleration(defautlAcceleration);
+  //stepper2.setSpeed(500); // steps per second
 
   //Screen setup
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -57,34 +65,101 @@ void setup() {
   delay(1000);
   Serial.println("setup complete");
 
-  stepper1.moveTo(stepsNeededForRevolution);
-  stepper2.moveTo(stepsNeededForRevolution);
+  stepper1.setCurrentPosition(0);
+  stepper2.setCurrentPosition(0);
 }
+
 
 void loop() {
-
-  if (!running) {
-    driving();
+  if (nextCommandNeeded && !running) {
+    executeCommand(currenctCommand);
+    newCommandPrint(currenctCommand);
     delay(1000);
     showSteps(stepsNeededForRevolution);
+    delay(20);
+  }
+
+
+  if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0) {
+    //stepper1.moveTo(-stepper1.currentPosition());
+    //stepper2.moveTo(-stepper1.currentPosition());
+    currenctCommand++;
+    running = 0;
   } else {
-    if (stepper1.distanceToGo() == 0) {
-      stepper1.moveTo(-stepper1.currentPosition());
-    }
-    if (stepper2.distanceToGo() == 0) {
-      stepper2.moveTo(-stepper2.currentPosition());
-    }
     stepper1.run();
     stepper2.run();
+    running = 1;
   }
-  running = 1;
+
+  if (currenctCommand > 5) {
+    currenctCommand = 0;
+  }
 }
 
-void driving(void) {
+// TODO: make better structure
+void executeCommand(int commandIndex) {
+Serial.print("executeCommand: ");
+Serial.println(commandIndex);
+
+  switch (commandIndex) {
+    case 0:
+      driveForwards();
+      break;
+    case 1:
+      driveBackwards();
+      break;
+    case 2:
+      turnLeft();
+      break;
+    case 3:
+      turnRight();
+      break;
+    case 4:
+      tankLeft();
+      break;
+    case 5:
+      tankRight();
+      break;
+    default:
+      Serial.println("Bad commandIndex");
+      break;
+  }
+}
+
+void driveForwards() {
+  stepper1.moveTo(stepper1.currentPosition() + stepsNeededForRevolution);
+  stepper2.moveTo(stepper2.currentPosition() + stepsNeededForRevolution);
+}
+
+void driveBackwards() {
+  stepper1.moveTo(stepper1.currentPosition() - stepsNeededForRevolution);
+  stepper2.moveTo(stepper2.currentPosition() - stepsNeededForRevolution);
+}
+
+void turnLeft() {
+  stepper1.moveTo(stepper1.currentPosition() + stepsNeededForRevolution);
+}
+
+void turnRight() {
+  stepper2.moveTo(stepper2.currentPosition() + stepsNeededForRevolution);
+}
+
+void tankLeft() {
+  stepper1.moveTo(stepper1.currentPosition() + stepsNeededForRevolution);
+  stepper2.moveTo(stepper2.currentPosition() - stepsNeededForRevolution);
+}
+
+void tankRight() {
+  stepper1.moveTo(stepper1.currentPosition() - stepsNeededForRevolution);
+  stepper2.moveTo(stepper2.currentPosition() + stepsNeededForRevolution);
+}
+
+void newCommandPrint(int commandIndex) {
   Serial.println("driving");
   display.clearDisplay();
   display.setCursor(16, 22);     // x,y
-  display.print("~DRIVIG~");
+  display.print("command: ");
+  display.print(commandIndex);
   display.display();
   delay(10);
 }
